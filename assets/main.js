601c5602e69667075a3a092b5220195aeccb2e06
Mani Sohal <mani@webenertia.com>
2023-07-10 11:57:41 +0530

    setup typography styles

--- assets/main.js
+++ assets/main.js
@@ -24,6 +24,30 @@ installCE(window, {
   noBuiltIn: true
 })
 
+// TODO: Find a better place to keep this code
+$(function () {
+  const wMin = 360
+  const wMax = 2500
+  const wDiff = wMax - wMin
+  const docRoot = document.querySelector(':root')
+  const typographySizeProp = '--window-size-anim-delay'
+
+  const typographySizing = function () {
+    const w = window.innerWidth
+    let ratio = (w - wMin) / wDiff
+    if (ratio <= 0) {
+      ratio = 0
+    } else if (ratio > 1) {
+      ratio = 1
+    }
+    ratio *= -1
+    docRoot.style.setProperty(typographySizeProp, ratio + 's')
+  }
+
+  typographySizing()
+  window.addEventListener('resize', typographySizing)
+})
+
 function importAll (r) {
   r.keys().forEach(r)
 }
