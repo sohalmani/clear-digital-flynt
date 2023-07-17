83a2be37331186a8a8ded95978639fd60d8ed500
Chander Shekhar <chander@webenertia.com>
2023-07-17 20:16:28 +0530

    feat: nav responsive js add with old way

--- Components/NavigationMain/script.js
+++ Components/NavigationMain/script.js
@@ -1,6 +1,4 @@
 import $ from 'jquery'
-import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
-
 class NavigationMain extends window.HTMLElement {
   constructor (...args) {
     const self = super(...args)
@@ -16,32 +14,94 @@ class NavigationMain extends window.HTMLElement {
   }
 
   bindFunctions () {
-    this.triggerMenu = this.triggerMenu.bind(this)
+    // this.triggerMenu = this.triggerMenu.bind(this)
+    // this.triggerMegaMenu = this.triggerMegaMenu.bind(this)
   }
 
   bindEvents () {
-    this.$.on('click', '[data-toggle-menu]', this.triggerMenu)
+    // this.$.on('click', '[data-toggle-menu]', this.triggerMenu)
+    // this.$.on('click', this.$navLink , this.triggerMegaMenu)
   }
 
   resolveElements () {
     this.$menu = $('.megamenu', this)
-    this.$menuButton = $('.hamburger', this)
     this.$navigation = $('.navigation', this)
+    this.$menuButton = $('.hamburger', this)
+    this.$nav = $('nav', this)
+    this.$navLink = $('.nav-link', this)
   }
 
   connectedCallback () {}
 
   triggerMenu (e) {
-    this.$.toggleClass('flyntComponent-menuIsOpen')
+    $(e.target).toggleClass('active')
+    this.$nav.slideToggle()
     this.$menuButton.attr('aria-expanded', this.$menuButton.attr('aria-expanded') === 'false' ? 'true' : 'false')
-    if (this.$.hasClass('flyntComponent-menuIsOpen')) {
-      disableBodyScroll(this.$menu.get(0))
-    } else {
-      enableBodyScroll(this.$menu.get(0))
-    }
+  }
+
+  triggerMegaMenu (e) {
+    this.$menu.removeClass('.nav-link--active')
+    $(e.target).toggleClass('nav-link--active')
+    $(e.target).find(this.$menu).slideToggle()
   }
 }
 
 window.customElements.define('flynt-navigation-main', NavigationMain, {
   extends: 'div'
 })
+
+// TODO: Convert code into ES6 classes base
+var $menu = $('.megamenu')
+var $navigation = $('.navigation')
+var $menuButton = $('.hamburger')
+var $nav = $('nav')
+var $navLink = $('.nav-link')
+
+if ($navigation) {
+  var initMenu = function () {
+    $menuButton.removeClass('active')
+    $navLink.removeClass('nav-link--active')
+    $nav.removeAttr('style')
+    $menu.removeClass('megamenu--open').removeAttr('style')
+  }
+
+  var triggerMenu = function (e) {
+    $(this).toggleClass('active')
+    $nav.slideToggle()
+    $menuButton.attr('aria-expanded', $menuButton.attr('aria-expanded') === 'false' ? 'true' : 'false')
+  }
+
+  var triggerMegaMenu = function (e) {
+    e.stopPropagation()
+
+    if ($(window).width() > 1023) return
+
+    if (!$(this).hasClass('nav-link--active')) {
+      $(this).siblings().removeClass('nav-link--active')
+      $(this).siblings().find('.megamenu__dropdown').stop(true, true).slideUp()
+      $(this).addClass('nav-link--active')
+      $(this).find('.megamenu__dropdown').stop(true, true).slideDown()
+    } else {
+      $navLink.removeClass('nav-link--active')
+      $navigation.find('.megamenu__dropdown').removeClass('megamenu--open').stop(true, true).slideUp()
+    }
+  }
+
+  var navTriggerScroll = function () {
+    var lastScrollVal = 0
+    var scrollValue = $(window).scrollTop()
+
+    if (scrollValue > lastScrollVal) {
+      $navigation.addClass('navigation--active')
+    } else if (scrollValue === 0) {
+      $navigation.removeClass('navigation--active')
+    }
+
+    lastScrollVal = scrollValue
+  }
+
+  $(window).on('scroll', navTriggerScroll)
+  $(window).on('orientationchange', initMenu)
+  $menuButton.on('click', triggerMenu)
+  $navLink.on('click', triggerMegaMenu)
+}
