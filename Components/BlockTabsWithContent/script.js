ad873d9e5bb53eac287e7b359f7e676638619165
Chander <chander@cleardigital.com>
2023-07-31 18:04:51 +0530

    fix: nav and tabs-with-content responsive

--- Components/BlockTabsWithContent/script.js
+++ Components/BlockTabsWithContent/script.js
@@ -5,19 +5,25 @@ $(function () {
   if ($tabsWithContent) {
     var $tabTitle = $tabsWithContent.find('.tab__head')
     var $tabBody = $tabsWithContent.find('.tabs-with-content__content .tab__body')
+    var $tabsItemsWrapper = $('.tabs-with-content__items')
     var $closeButton = $tabsWithContent.find('.close-btn')
     var addIdOnInnerMenu = function (elements, customAttribute, attrTextValue) {
       $(elements).each(function (i) {
         $(this).attr(customAttribute, attrTextValue + (i + 1))
       })
     }
+    var initTabs = function () {
+      $('.tab').removeClass('tab--open')
+      $tabsWithContent.find('.tab__body').removeAttr('style')
+      $tabsItemsWrapper.removeAttr('style')
+    }
     var handleTabClick = function (e) {
       var tabId = $(this).attr('data-tab')
       var windowWidth = $(window).width()
 
       if (windowWidth > 767) {
-        $(this).closest('.tabs-with-content__items').fadeOut()
-        $(this).closest('.tabs-with-content').find('.tabs-with-content__content .tab__body[data-tab-body=' + tabId + ']').fadeIn()
+        $(this).closest('.tabs-with-content__items').css({ display: 'none' })
+        $(this).closest('.tabs-with-content').find('.tabs-with-content__content .tab__body[data-tab-body=' + tabId + ']').css({ display: 'block' })
       } else {
         if (!$(this).closest('.tab').hasClass('tab--open')) {
           $('.tab').removeClass('tab--open')
@@ -31,12 +37,13 @@ $(function () {
       }
     }
     var handleCloseButton = function (e) {
-      $(this).closest('.tab__body').fadeOut()
-      $(this).closest('.tabs-with-content').find('.tabs-with-content__items').fadeIn()
+      $(this).closest('.tab__body').css({ display: 'none' })
+      $(this).closest('.tabs-with-content').find('.tabs-with-content__items').css({ display: 'block' })
     }
     addIdOnInnerMenu($tabTitle, 'data-tab', 'tab-')
     addIdOnInnerMenu($tabBody, 'data-tab-body', 'tab-')
     $tabTitle.on('click', handleTabClick)
     $closeButton.on('click', handleCloseButton)
+    $(window).on('resize', initTabs)
   }
 })
